8ae7389fa4fcbf204e1f05e5384c703004f938da
Alejandro Parra <alejandro_parra_15@hotmail.com>
2022-01-23 21:44:18 -0400

    feat: Dynamic data from backend

--- src/components/FilesTable.js
+++ src/components/FilesTable.js
@@ -1,11 +1,38 @@
+import { useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 import Table from 'react-bootstrap/Table';
 import FilesTableHeader from './FilesTableHeader';
 import FilesTableItems from './FilesTableItems';
+import ShifyService from '../services/Shify.service';
 
 const FilesTable = () => {
+  const [fileNames, setFileNames] = useState([]);
+  const [isFetchingData, setIsFetchingData] = useState();
+  const [errorFetching, setErrorFetching] = useState({
+    value: false,
+    msg: '',
+  });
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        setIsFetchingData(true);
+        const filesData = await ShifyService.getFilesData();
+        setFileNames(filesData);
+        setIsFetchingData(false);
+      } catch (error) {
+        setErrorFetching({
+          value: true,
+          msg: error.message,
+        });
+      }
+    };
+
+    getData();
+  }, []);
+
   return (
     <Row>
       <Col>
@@ -13,32 +40,9 @@ const FilesTable = () => {
           <Table striped bordered hover>
             <FilesTableHeader items={['File Name', 'Text', 'Number', 'Hex']} />
             <FilesTableItems
-              fileNames={[
-                {
-                  name: 'test1.csv',
-                  text: 'sdv',
-                  number: 235235,
-                  hex: 'ounoivn',
-                },
-                {
-                  name: 'test1.csv',
-                  text: 'fdbdfb',
-                  number: 346346436,
-                  hex: 'dfbdfbfd',
-                },
-                {
-                  name: 'test2.csv',
-                  text: 'dfhdfhdfha',
-                  number: 235235,
-                  hex: 'wetwettwet',
-                },
-                {
-                  name: 'test2.csv',
-                  text: 'weeeweet',
-                  number: 77457654,
-                  hex: 'yulyulyul',
-                },
-              ]}
+              fileNames={fileNames}
+              isFetchingData={isFetchingData}
+              errorFetching={errorFetching}
             />
           </Table>
         </Container>
