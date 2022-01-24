e11284c953f7b3cb662b5915ebaf52f73d1cbe98
Alejandro Parra <alejandro_parra_15@hotmail.com>
2022-01-23 20:00:51 -0400

    feat: UI without components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { Col, Container, Row, Table } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container fluid>
+        <Row className="bg-danger text-white py-3 mb-4">
+          <Col>
+            <h3 className="fw-bold">React Test App</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <Container>
+              <Table striped bordered hover>
+                <thead>
+                  <tr>
+                    <th>File Name</th>
+                    <th>Text</th>
+                    <th>Number</th>
+                    <th>Hex</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td colSpan={2}>Larry the Bird</td>
+                    <td>@twitter</td>
+                  </tr>
+                </tbody>
+              </Table>
+            </Container>
+          </Col>
+        </Row>
+      </Container>
+    </>
   );
 }
 
