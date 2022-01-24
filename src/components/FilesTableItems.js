4f4df81e0d25e425392c1ed8ae98f7e9b60b6d27
Alejandro Parra <alejandro_parra_15@hotmail.com>
2022-01-23 23:27:30 -0400

    refactor: Improve readibility of FilesTableItems component using an ErrorBoundary

--- src/components/FilesTableItems.js
+++ src/components/FilesTableItems.js
@@ -4,27 +4,23 @@ const FilesTableItems = ({ fileNames, isFetchingData, errorFetching }) => {
   return (
     <tbody>
       <IsFetchingFilesTablesItems isFetching={isFetchingData}>
-        {errorFetching.value ? (
-          <tr>
-            <td colSpan={4} className="text-center fw-bold py-5">
-              {errorFetching.msg}
-            </td>
-          </tr>
-        ) : fileNames.length > 0 ? (
-          fileNames.map((fileName, index) => (
-            <FilesTableItem
-              key={index}
-              fileName={fileName.name}
-              text={fileName.text}
-              number={fileName.number}
-              hex={fileName.hex}
-            />
-          ))
-        ) : (
-          <tr>
-            <td colSpan={4}>Data could not be got from service</td>
-          </tr>
-        )}
+        <ErrorBoundaryFilesTablesItems error={errorFetching}>
+          {fileNames.length > 0 ? (
+            fileNames.map((fileName, index) => (
+              <FilesTableItem
+                key={index}
+                fileName={fileName.name}
+                text={fileName.text}
+                number={fileName.number}
+                hex={fileName.hex}
+              />
+            ))
+          ) : (
+            <tr>
+              <td colSpan={4}>Data could not be got from service</td>
+            </tr>
+          )}
+        </ErrorBoundaryFilesTablesItems>
       </IsFetchingFilesTablesItems>
     </tbody>
   );
@@ -37,6 +33,22 @@ const IsFetchingFilesTablesItems = ({ isFetching, children }) => {
         <tr>
           <td colSpan={4} className="text-center py-5">
             Loading...
+          </td>
+        </tr>
+      ) : (
+        children
+      )}
+    </>
+  );
+};
+
+const ErrorBoundaryFilesTablesItems = ({ error, children }) => {
+  return (
+    <>
+      {error.value ? (
+        <tr>
+          <td colSpan={4} className="text-center fw-bold py-5">
+            {error.msg}
           </td>
         </tr>
       ) : (
